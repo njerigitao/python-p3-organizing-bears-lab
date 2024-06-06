804c27e3d9d64cb25c145d31b048cfae511b29d3
AGNESGITAU <agnesgitau40@gmail.com>
2024-06-06 10:05:16 +0300

    INSERT VALUES

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears  VALUES (1, 'Mr.Chocolate', 8, 'M', 'Black', 'synical', 1);
+INSERT INTO bears  VALUES (2, 'Rowdy', 12, 'F', 'White', 'aggressive', 1);
+INSERT INTO bears  VALUES (3, 'Tabitha', 5, 'F', 'Brown', 'calm', 0);
+INSERT INTO bears  VALUES (4, 'Seargeant Brown', 20, 'M', 'Brown', 'stern', 1);
+INSERT INTO bears  VALUES (5, 'Melissa', 16, 'F', 'Grey', 'friendly', 1);
+INSERT INTO bears  VALUES (6, 'Grinch', 25, 'M', 'Green', 'cunning', 1);
+INSERT INTO bears  VALUES (7, 'Wendy', 5, 'F', 'White', 'sweet', 0);
+INSERT INTO bears  VALUES (8, null, 13, 'F', 'Black', 'naive', 0);
